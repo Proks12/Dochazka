e25309d2e2d346d4b91846aa8088a0f847c02885
marti <martinproks06@seznam.cz>
2026-01-03 20:41:15 +0100

    přidání Main pro test zápisu docházky

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import model.StavDochazky;
+import service.DochazkaService;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        DochazkaService service = new DochazkaService();
+
+        service.zapsatDochazku(
+                1,              // id_student
+                1,              // id_vyucovani
+                StavDochazky.PRITOMEN,
+                "Včasný příchod"
+        );
+
+        System.out.println("Hotovo.");
     }
 }
